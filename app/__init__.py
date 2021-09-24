fd1a0ce80f4e49e5d9a968d35683ccb2369360ec
Alan Marques Sosa <alan.sousa@protonmail.com>
2021-09-23 22:44:51 -0300

    Criacao da rota de upload de pdf

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,33 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'pdf'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def index():
   return "Hello world!"
+
+@app.route("/upload-pdf", methods=['POST'])
+def process():
+  if request.method == 'POST' or file.filename == '':
+    if 'file' not in request.files:
+      return 'No file to upload'
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      return 'File uploaded successfully'
+  return ''
+
 
 if __name__ == "__app__":
   app.run()
